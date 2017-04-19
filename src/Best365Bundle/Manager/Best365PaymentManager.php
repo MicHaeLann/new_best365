1568d4fcb505054d352297769bc031d8ae4932cf
MicHaeLann <398326042@qq.com>
2017-04-19 14:48:41 +1200

    add debug data for poli

--- src/Best365Bundle/Manager/Best365PaymentManager.php
+++ src/Best365Bundle/Manager/Best365PaymentManager.php
@@ -301,9 +301,19 @@ class Best365PaymentManager
 		  "NotificationURL":  "' . $this->router->generate('best365_store_poli', array(), UrlGeneratorInterface::ABSOLUTE_URL) . '"
 		}';
 
-		ladybug_dump($this->router->generate('best365_store_order_list_error', array(), UrlGeneratorInterface::ABSOLUTE_URL));
-		ladybug_dump($this->router->generate('best365_store_poli', array(), UrlGeneratorInterface::ABSOLUTE_URL));
-		ladybug_dump($json_builder);
+		$json_builder = '{
+		  "Amount": "' . $amount->getAmount() / 100 . '",
+		  "CurrencyCode": "NZD",
+		  "MerchantReference": "' . $order->getId() . '",
+		  "MerchantHomepageURL": "https://www.best365.co.nz",
+		  "SuccessURL": "https://www.baidu.com",
+		  "FailureURL": "https://www.google.co.nz",
+		  "CancellationURL": "https://www.google.hk",
+		  "NotificationURL":  "https://www.google.co.uk"
+		}';
+//		ladybug_dump($this->router->generate('best365_store_order_list_error', array(), UrlGeneratorInterface::ABSOLUTE_URL));
+//		ladybug_dump($this->router->generate('best365_store_poli', array(), UrlGeneratorInterface::ABSOLUTE_URL));
+//		ladybug_dump($json_builder);
 
 		$auth = base64_encode($this->poli->merchant_code . ':' . $this->poli->auth_code);
 		$header = array();
@@ -321,7 +331,7 @@ class Best365PaymentManager
 		$response = curl_exec($ch);
 		curl_close($ch);
 		$json = json_decode($response, true);
-		ladybug_dump($json);exit;
+//		ladybug_dump($json);exit;
 
 		return $json;
 	}
