cb7f67c897a73fd6758241fb11a7f3ad3481f4d0
MicHaeLann <398326042@qq.com>
2017-05-24 16:31:17 +1200

    remove dump from validator

--- vendor/elcodi/elcodi/src/Elcodi/Component/Geo/Validator/Constraints/CityExistsValidator.php
+++ vendor/elcodi/elcodi/src/Elcodi/Component/Geo/Validator/Constraints/CityExistsValidator.php
@@ -66,8 +66,6 @@ class CityExistsValidator extends ConstraintValidator
         } catch (EntityNotFoundException $e) {
             $location = null;
         }
-        ladybug_dump($location);
-		ladybug_dump(!($location instanceof LocationData));
         if (
             !($location instanceof LocationData) ||
             'city' != $location->getType()
